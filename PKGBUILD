28346d567a004dc8bd603531125c3097fa87d5f1
ghost <>
2025-02-19 10:46:45 +0100

    Make all types and their fields private

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 pkgname=ovpn-status
-pkgver=0.3.1
-pkgrel=4
+pkgver=0.3.2
+pkgrel=1
 pkgdesc='Queries the endpoints used by OVPN's status site and shows all servers' status in the terminal'
 arch=('x86_64')
 url="https://git.nospy.in/Rust/$pkgname"
--- ovpn_status.rs
+++ ovpn_status.rs
@@ -58,58 +58,58 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct APIResponse {
-    pub success: bool,
-    pub datacenters: Vec<DataCenter>,
-    pub shadowsocks: ShadowSocks,
+struct APIResponse {
+    success: bool,
+    datacenters: Vec<DataCenter>,
+    shadowsocks: ShadowSocks,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct DataCenter {
-    pub slug: String,
-    pub city: String,
-    pub country: String,
-    pub country_name: String,
-    pub pools: Vec<String>,
-    pub ping_address: Ipv4Addr,
-    pub servers: Vec<Server>,
+struct DataCenter {
+    slug: String,
+    city: String,
+    country: String,
+    country_name: String,
+    pools: Vec<String>,
+    ping_address: Ipv4Addr,
+    servers: Vec<Server>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct Server {
-    pub ip: Ipv4Addr,
-    pub ptr: String,
-    pub name: String,
-    pub online: bool,
-    pub load: u8,
-    pub public_key: String,
-    pub public_key_ipv4: String,
-    pub wireguard_ports: Vec<u16>,
-    pub multihop_openvpn_port: u16,
-    pub multihop_wireguard_port: u16,
+struct Server {
+    ip: Ipv4Addr,
+    ptr: String,
+    name: String,
+    online: bool,
+    load: u8,
+    public_key: String,
+    public_key_ipv4: String,
+    wireguard_ports: Vec<u16>,
+    multihop_openvpn_port: u16,
+    multihop_wireguard_port: u16,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct ShadowSocks {
-    pub mode: String,
-    pub method: String,
-    pub password: String,
-    pub ports: Vec<u16>,
+struct ShadowSocks {
+    mode: String,
+    method: String,
+    password: String,
+    ports: Vec<u16>,
 }
 
 // per city
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct StatusResponse {
-    pub data: Vec<ServerInfo>,
+struct StatusResponse {
+    data: Vec<ServerInfo>,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
-pub struct ServerInfo {
-    pub online: bool,
-    pub uptime: String,
-    pub bandwidth: u16,
-    pub bandwidth_usage: u8,
-    pub port_speed: u16,
-    pub name: String,
-    pub ip: Ipv4Addr,
+struct ServerInfo {
+    online: bool,
+    uptime: String,
+    bandwidth: u16,
+    bandwidth_usage: u8,
+    port_speed: u16,
+    name: String,
+    ip: Ipv4Addr,
 }
